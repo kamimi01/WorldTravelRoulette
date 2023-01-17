e704649c71feaf5e1679efb46a87d6b8d3414e1a
mika.urakawa <awakaru0122@gmail.com>
2023-01-18 06:49:35 +0900

    add: doc for privacy_policy

---
+++ WorldTravelRoulette/WorldTravelRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldTravelRouletteApp.swift
+//  WorldTravelRoulette
+//
+//  Created by mikaurakawa on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct WorldTravelRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
