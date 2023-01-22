1530b834d54fc360a237e506c5e8ba264455c5dd
mika.urakawa <awakaru0122@gmail.com>
2023-01-22 21:12:35 +0900

    fix: roulette view

--- WorldTravelRoulette/Roulette/RouletteScreen.swift
+++ WorldTravelRoulette/Roulette/RouletteScreen.swift
@@ -20,53 +20,58 @@ struct RouletteScreen: View {
     )
 
     var body: some View {
-
         NavigationView {
-            VStack(spacing: 20) {
-                switch viewModel.rouletteStatus {
-                case .rolling:
-                    Spacer()
-                    LottieView(animationType: .oneTwoThree)
-                        .frame(width: 100)
-                case .notRolling, .endRolling:
-                    VStack(alignment: .center, spacing: 5) {
-                        if let selectedCountry = viewModel.selectedCountry {
-                            HStack(spacing: 10) {
-                                Text(selectedCountry.commonNameJa ?? selectedCountry.commonName)
-                                    .font(.title)
-                                AsyncImage(url: URL(string: selectedCountry.flagPng)) { image in
-                                    image
-                                        .resizable()
-                                        .scaledToFit()
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                .frame(width: 60)
-                            }
-                            Text("\nにいこう！")
-                                .font(.title2)
-                            Map(coordinateRegion: $region)
-                        } else {
-                            Text("全部行ったね！すごい！🎉")
-                                .font(.title)
-                        }
-                    }
+            ZStack {
+                Map(coordinateRegion: $region)
                     .frame(maxWidth: .infinity)
                     .frame(maxHeight: .infinity)
-                    .border(Color.red)
-                }
-                Spacer()
-                Button(action: {
-                    viewModel.startRoulette()
-                }) {
-                    Text("旅をする")
-                        .font(.title)
-                        .frame(width: 200, height: 200)
-                        .imageScale(.large)
-                        .background(Color.green)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                        .shadow(color: .gray, radius: 3, x: 5, y: 5)
+                VStack {
+                    switch viewModel.rouletteStatus {
+                    case .rolling:
+                        Spacer()
+                        LottieView(animationType: .oneTwoThree)
+                            .frame(width: 100)
+                        Spacer()
+                    case .notRolling, .endRolling:
+                        VStack(alignment: .leading, spacing: 5) {
+                            if let selectedCountry = viewModel.selectedCountry {
+                                HStack(spacing: 10) {
+                                    AsyncImage(url: URL(string: selectedCountry.flagPng)) { image in
+                                        image
+                                            .resizable()
+                                            .scaledToFit()
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                    .frame(width: 60)
+                                    Text(selectedCountry.commonNameJa ?? selectedCountry.commonName)
+                                        .font(.title)
+                                }
+                            } else {
+                                Text("全部行ったね！すごい！🎉")
+                                    .font(.title)
+                            }
+                        }
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .padding(.horizontal, 16)
+                        .padding(.top, 16)
+                    }
+                    Spacer()
+                    Button(action: {
+                        viewModel.startRoulette()
+                    }) {
+                        Text("旅をする")
+                            .font(.title)
+                            .frame(width: 150, height: 150)
+                            .imageScale(.large)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                            .shadow(color: .gray, radius: 3, x: 5, y: 5)
+                    }
                 }
             }
             .navigationTitle("次はどこへ行く？")
