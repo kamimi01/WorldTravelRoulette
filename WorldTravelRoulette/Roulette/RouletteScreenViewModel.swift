1714eb063b116d11a937428f2e97437d0c13d294
mika.urakawa <awakaru0122@gmail.com>
2023-01-23 06:48:51 +0900

    add: select random country

--- WorldTravelRoulette/Roulette/RouletteScreenViewModel.swift
+++ WorldTravelRoulette/Roulette/RouletteScreenViewModel.swift
@@ -42,21 +42,45 @@ class RouletteScreenViewModel: ObservableObject {
             self.rouletteStatus = .endRolling
 
             // TODO: 仮実装
-            self.selectedCountry = Country(commonName: "british", commonNameJa: "イギリス", flagPng: "https://flagcdn.com/w320/zm.png", googleMapURL: "https://goo.gl/maps/mweBcqvW8TppZW6q9", capitalLocation: Location(lat: 51.5, lng: -0.08))
+//            self.selectedCountry = Country(commonName: "british", commonNameJa: "イギリス", flagPng: "https://flagcdn.com/w320/zm.png", googleMapURL: "https://goo.gl/maps/mweBcqvW8TppZW6q9", capitalLocation: Location(lat: 51.5, lng: -0.08))
+//            self.selectedRegion = MKCoordinateRegion(
+//                center: CLLocationCoordinate2D(
+//                    latitude: 51.5,
+//                    longitude: -0.08
+//                ),
+//                latitudinalMeters: 1000 * 1000,
+//                longitudinalMeters: 1000 * 1000
+//            )
+
+            // 国を取得する
+            guard let countries = DataManager.loadCachedData() else {
+                print("no any country")
+                return
+            }
+
+            // 行ったことのない国を取得
+            var haventBeenCountries = countries.filter { $0.isSelected == false }
+
+            // 行ったことのない国からランダムで選択する
+            let selectedIndex = Int.random(in: 1..<haventBeenCountries.count + 1)
+            guard let nextCountry = haventBeenCountries[safe: selectedIndex] else {
+                return
+            }
+            self.selectedCountry = nextCountry
+            guard let lat = self.selectedCountry.capitalLocation.lat,
+                  let lng = self.selectedCountry.capitalLocation.lng
+            else {
+                print("no lat or lng")
+                return
+            }
             self.selectedRegion = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(
-                    latitude: 51.5,
-                    longitude: -0.08
+                    latitude: lat,
+                    longitude: lng
                 ),
                 latitudinalMeters: 1000 * 1000,
                 longitudinalMeters: 1000 * 1000
             )
-
-            // 行ったことがある国を取得する
-
-            // 行ったことのない国を取得
-
-            // 行ったことのない国からランダムで選択する
         }
     }
 }
